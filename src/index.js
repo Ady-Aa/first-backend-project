a0c1b9eefaea6b6d3b1f58ea708a2c1cdcf0f66a
Aaditya <aaditya97547@gmail.com>
2026-03-07 18:37:24 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 // require('dotenv').config()
 import dotenv from "dotenv";
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 import connectDB from "./db/db.js";
+import express from 'express'
 
-dotenv.config();
 
-connectDB();
+const app = express()
+
+dotenv.config({
+    path: "./.env"
+});
+
+
+const listenAt = process.env.PORT || 8000
+
+
+connectDB()
+.then(() => {
+    app.listen(listenAt, (req, res) => {
+        console.log(`Server is running at ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err)
+})
 
 /*
 import express from "express";
